100c00dd232e151676fce0145e01bd840a5f15ff
LeoMangot5 <leokuo@mangot5.com>
2025-06-20 15:10:51 +0800

    讀取 excel

--- main.go
+++ main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/xuri/excelize/v2"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	f, err := excelize.OpenFile("stage.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	// 取得第一個工作表名稱
+	sheetName := f.GetSheetName(0)
+
+	// 讀取指定工作表所有資料
+	rows, err := f.GetRows(sheetName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 遍歷所有列與欄
+	for i, row := range rows {
+		fmt.Printf("Row %d: ", i+1)
+		for j, cell := range row {
+			fmt.Printf("(%d,%d) %s  ", i+1, j+1, cell)
+		}
+		fmt.Println()
 	}
 }
